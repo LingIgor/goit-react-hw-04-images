45a7ef4c6f8cc0f2cb029861276f808f69d7bcc3
Ling Igor <lingigor1990@gmail.com>
2023-05-20 21:31:13 +0300

    commmit with hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,85 @@
 // import axios from 'axios';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 
-const status = {
+const stat = {
   IDLE: 'idle',
   PENDING: 'pending',
   RESOLVED: 'resolved',
   REJECTED: 'rejected',
 };
 
-export class App extends Component {
-  state = {
-    status: 'idle',
-    images: [],
-    query: null,
-    page: 1,
-    per_page: 12,
-    error: null,
-    isLoader: false,
-  };
+export const App = () => {
+  const [status, setStatus] = useState('idle');
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState(null);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [isLoader, setIsLoader] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({
-        isLoader: true,
-      });
+  useEffect(
+    prev => {
+      if (prev !== query) {
+        setIsLoader(true);
+        fetchFn();
+      }
+    },
+    [page, query]
+  );
 
-      this.fetchFn();
-    }
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   const { query, page } = this.state;
+  //   if (prevState.query !== query || prevState.page !== page) {
+  //     this.setState({
+  //       isLoader: true,
+  //     });
 
-  fetchFn = () => {
-    const { query, page, per_page } = this.state;
+  //     this.fetchFn();
+  //   }
+  // }
+
+  const fetchFn = () => {
+    const per_page = 12;
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '34770322-1d785185ad6fb3686a5689e8d';
     const ALL_URL = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
 
     fetch(ALL_URL)
       .then(res => res.json())
-      .then(({ hits }) =>
-        this.setState(({ images }) => ({
-          images: [...images, ...hits],
-          isLoader: false,
-        }))
-      )
-      .catch(error => this.setState({ error, status: status.REJECTED }))
-      .finally(() => this.setState({ status: status.RESOLVED }));
+      .then(({ hits }) => {
+        setImages([...images, ...hits]);
+        setIsLoader(false);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus(stat.REJECTED);
+      })
+
+      .finally(() => setStatus(stat.RESOLVED));
   };
 
-  onBtnClickPg = () => {
-    this.setState(({ page }) => ({
-      page: page + 1,
-    }));
+  const onBtnClickPg = () => {
+    setPage(page + 1);
   };
 
-  onSubmit = value => {
-    this.setState({
-      query: value,
-      page: 1,
-      images: [],
-    });
+  const onSubmit = value => {
+    setQuery(value);
+    setPage(1);
+    setImages([]);
   };
 
-  render() {
-    const { onSubmit, onBtnClickPg } = this;
-    return (
-      <>
-        <Searchbar onSubmit={onSubmit} />
-        {this.state.status === 'idle' && <div>Введіть значення</div>}
-        {this.state.isLoader && <Loader />}
-        {this.state.status === 'resolved' && (
-          <ImageGallery images={this.state.images} />
-        )}
-        {this.state.status === 'rejected' && (
-          <h1>{this.state.error.message}</h1>
-        )}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmit} />
+      {status === 'idle' && <div>Введіть значення</div>}
+      {isLoader && <Loader />}
+      {status === 'resolved' && <ImageGallery images={images} />}
+      {status === 'rejected' && <h1>{error.message}</h1>}
 
-        {this.state.images.length !== 0 && <Button onClick={onBtnClickPg} />}
-      </>
-    );
-  }
-}
+      {images.length !== 0 && <Button onClick={onBtnClickPg} />}
+    </>
+  );
+};
