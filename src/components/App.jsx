86268927334d67cbafee454afc02c42c6566c49b
Ling Igor <lingigor1990@gmail.com>
2023-05-20 21:08:51 +0300

    dsds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,87 @@
-export const App = () => {
-  useEffect(() => {
-    if (query === '') return;
-    console.log('use');
-    setIsLoader(true);
-    fetchFn();
-  }, [query, page]);
+// import axios from 'axios';
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
 
-  const fetchFn = () => {
+const status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
+export class App extends Component {
+  state = {
+    status: 'idle',
+    images: [],
+    query: null,
+    page: 1,
+    per_page: 12,
+    error: null,
+    isLoader: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({
+        isLoader: true,
+      });
+
+      this.fetchFn();
+    }
+  }
+
+  fetchFn = () => {
+    const { query, page, per_page } = this.state;
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '34770322-1d785185ad6fb3686a5689e8d';
     const ALL_URL = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
 
     fetch(ALL_URL)
       .then(res => res.json())
-      .then(({ hits }) => {
-        setImages([...images, ...hits]);
-        setIsLoader(false);
-      })
-      .catch(error => {
-        setError(error);
-        setStatus(stat.REJECTED);
-      })
-      .finally(() => setStatus(stat.RESOLVED));
+      .then(({ hits }) =>
+        this.setState(({ images }) => ({
+          images: [...images, ...hits],
+          isLoader: false,
+        }))
+      )
+      .catch(error => this.setState({ error, status: status.REJECTED }))
+      .finally(() => this.setState({ status: status.RESOLVED }));
   };
 
-  const onBtnClickPg = () => {
-    setPage(page + 1);
+  onBtnClickPg = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
   };
 
-  const onSubmit = value => {
-    setQuery(value);
-    setPage(1);
-    setImages([]);
+  onSubmit = value => {
+    this.setState({
+      query: value,
+      page: 1,
+      images: [],
+    });
   };
 
-  return (
-    <>
-      <Searchbar onSubmit={onSubmit} />
-      {stat === 'idle' && <div>Введіть значення</div>}
-      {isloader && <Loader />}
-      {stat === 'resolved' && <ImageGallery images={images} />}
-      {stat === 'rejected' && <h1>{error.message}</h1>}
+  render() {
+    const { onSubmit, onBtnClickPg } = this;
+    return (
+      <>
+        <Searchbar onSubmit={onSubmit} />
+        {this.state.status === 'idle' && <div>Введіть значення</div>}
+        {this.state.isLoader && <Loader />}
+        {this.state.status === 'resolved' && (
+          <ImageGallery images={this.state.images} />
+        )}
+        {this.state.status === 'rejected' && (
+          <h1>{this.state.error.message}</h1>
+        )}
 
-      {images.length !== 0 && images.length / 12 === page && (
-        <Button onClick={onBtnClickPg} />
-      )}
-    </>
-  );
-};
+        {this.state.images.length !== 0 && <Button onClick={onBtnClickPg} />}
+      </>
+    );
+  }
+}
