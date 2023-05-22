6d1cf6a793a4fd6f787eb34f127bf22f6292abe8
LingIgor <lingigor1990@gmail.com>
2023-05-22 11:23:10 +0300

    commit App hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 // import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Api from '../components/API/API';
+
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
@@ -14,59 +14,38 @@ const stat = {
 };
 
 export const App = () => {
-  const [status, setStatus] = useState('idle');
+  const [status, setStatus] = useState(stat.IDLE);
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState(null);
   const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
-  const [isLoader, setIsLoader] = useState(false);
-
-  // useEffect(() => {
-  //   if (!query) {
-  //     return;
-  //   }
-  //   setIsLoader(true);
-  //   Api.fetchFn(page, query)
-  //     .then(({ hits }) => setImages(prevState => [...prevState, ...hits]))
-  //     .catch(error => {
-  //       setError(error);
-  //       setStatus(stat.REJECTED);
-  //     });
-
-  //   setIsLoader(false);
-  // }, [page, query]);
 
   useEffect(() => {
     if (!query) {
       return;
     }
 
-    const renderImages = () => {
+    const fetchFn = () => {
       setStatus(stat.PENDING);
-      setIsLoader(true);
-      Api.fetchFn(query, page)
-        .then(data => setImages(prevState => [...prevState, ...data.hits]))
+       const per_page = 12
+      const BASE_URL = 'https://pixabay.com/api/';
+      const API_KEY = '34770322-1d785185ad6fb3686a5689e8d';
+      const ALL_URL = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`;
+      
+      fetch(ALL_URL)
+        .then(res => res.json())
+        .then(({ hits }) =>
+          setImages( images  => [...images, ...hits]))          
         .catch(error => {
-          setError(error);
-          setStatus(stat.REJECTED);
-        })
-        .finally(() => setStatus(stat.RESOLVED));
+          setError(error)
+        setStatus(stat.REJECTED)})
+        .finally(() => setStatus(stat.RESOLVED ));
     };
-    setIsLoader(false);
-    renderImages();
-    // scroll.scrollToBottom();
+    
+    fetchFn ()   
   }, [query, page]);
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   const { query, page } = this.state;
-  //   if (prevState.query !== query || prevState.page !== page) {
-  //     this.setState({
-  //       isLoader: true,
-  //     });
 
-  //     this.fetchFn();
-  //   }
-  // }
 
   const onBtnClickPg = () => {
     setPage(page + 1);
@@ -82,10 +61,9 @@ export const App = () => {
     <>
       <Searchbar onSubmit={onSubmit} />
       {status === 'idle' && <div>Введіть значення</div>}
-      {isLoader && <Loader />}
+      {status === stat.PENDING && (<Loader/>)}
       {status === 'resolved' && <ImageGallery images={images} />}
       {status === 'rejected' && <h1>{error.message}</h1>}
-
       {images.length !== 0 && <Button onClick={onBtnClickPg} />}
     </>
   );
